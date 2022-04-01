bf0417e4383e1bc2704b08c41d64bb825d43e033
Eyad Kobatte <ekobatte96@gmail.com>
2022-04-01 12:39:18 +0100

    upgrade analytics google

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,12 +49,6 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        trackingId: `UA-128187782-2`,
-      },
-    },
-    {
       resolve: 'gatsby-plugin-manifest',
       options: {
         name: 'DeveloPanda',
@@ -75,5 +69,30 @@ module.exports = {
       },
     },
     'gatsby-plugin-image',
+    {
+      resolve: 'gatsby-plugin-google-gtag',
+      options: {
+        trackingIds: [
+          'G-Y6QB8BTRFY', // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ['/preview/**'],
+          // Defaults to https://www.googletagmanager.com
+          origin: 'YOUR_SELF_HOSTED_ORIGIN',
+        },
+      },
+    },
   ],
 };
